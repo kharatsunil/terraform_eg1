93770fcdd4c08dec89cd75ff6fa113671af9b466
kharatsunil <44827802+kharatsunil@users.noreply.github.com>
2018-11-14 14:05:27 +1100

    Delete provider.tf

--- tf/Test/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-    region = "${var.aws_region}"
-    access_key = "AKIAI2HDBAMKJIB2BSGA"
-    secret_key = "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"
-    profile = "terraform"
-}
